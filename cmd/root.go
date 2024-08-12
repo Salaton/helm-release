d7549c9c654891bb4fb76adb2da518d1ce572851
Salaton <nairouasalaton@gmail.com>
2024-08-12 22:18:51 +0300

    feat: setup cobra cli & viper

---
+++ cmd/root.go
@@ -0,0 +1,72 @@
+/*
+Copyright © 2024 Nairoua Salaton nairouasalaton@gmail.com
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "helm-release",
+	Short: "A CLI tool to automate version management and releases for Helm charts",
+	Long: `helm-release is a command-line tool designed to streamline the process of managing and releasing Helm charts. 
+Helm-release simplifies the versioning process by automatically determining and applying the appropriate version bump based on changes made to your charts. 
+`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.helm-release.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".helm-release" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".helm-release")
+	}
+
+	viper.AutomaticEnv()
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2024 Nairoua Salaton nairouasalaton@gmail.com
+*/
+package main
+
+import "github.com/Salaton/helm-release/cmd"
+
+func main() {
+	cmd.Execute()
+}
